3f4d36d497ab805858a8a1ece1afcccb6fdbec24
FabienD0 <fabien.denommee@gmail.com>
2023-04-01 14:58:30 -0400

    indexAddedPatchMethod

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ express()
   .use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Methods",
-      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+      "OPTIONS, HEAD, GET, PUT, POST,PATCH, DELETE"
     );
     res.header(
       "Access-Control-Allow-Headers",
