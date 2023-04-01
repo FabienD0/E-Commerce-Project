bb697f3d87bde22d554bcefebb7773b508796d5e
FabienD0 <fabien.denommee@gmail.com>
2023-04-01 12:48:53 -0400

    portFixed

--- server/index.js
+++ server/index.js
@@ -19,8 +19,7 @@ const {
   getItemsByCategory,
 } = require("./handlers");
 
-// const PORT = 8080;
-const PORT = "https://e-commerce-project-dvgs.onrender.com";
+const PORT = 8080;
 
 express()
   .use(function (req, res, next) {
