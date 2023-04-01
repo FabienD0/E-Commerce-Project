ccc2f35f4d9816351c90cb5af56ced590b00cc42
FabienD0 <fabien.denommee@gmail.com>
2023-04-01 14:53:56 -0400

    indexupdate

--- server/index.js
+++ server/index.js
@@ -23,14 +23,14 @@ const PORT = process.env.PORT || 8080;
 
 express()
   .use(function (req, res, next) {
-    // res.header(
-    //   "Access-Control-Allow-Methods",
-    //   "OPTIONS, HEAD, GET, PUT, POST, DELETE"
-    // );
-    // res.header(
-    //   "Access-Control-Allow-Headers",
-    //   "Origin, X-Requested-With, Content-Type, Accept"
-    // );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     res.header("Access-Control-Allow-Origin", "*");
     next();
   })
